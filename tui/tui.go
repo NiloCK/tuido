ba15fd6afbce4b71a1f31a4cd619caa8d6190419
Colin Kennedy <colin@mrkennedy.ca>
2022-04-28 23:13:45 -0300

    pull view fcns to own file

--- tui/tui.go
+++ tui/tui.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/nilock/tuido/tuido"
 )
 
@@ -145,86 +144,6 @@ func (t *tui) populateRenderSelection() {
 }
 
 func (t tui) Init() tea.Cmd { return nil }
-
-var (
-	activeTabBorder = lipgloss.Border{
-		Top:         "─",
-		Bottom:      " ",
-		Left:        "│",
-		Right:       "│",
-		TopLeft:     "╭",
-		TopRight:    "╮",
-		BottomLeft:  "┘",
-		BottomRight: "└",
-	}
-
-	tabBorder = lipgloss.Border{
-		Top:         "─",
-		Bottom:      "─",
-		Left:        "│",
-		Right:       "│",
-		TopLeft:     "╭",
-		TopRight:    "╮",
-		BottomLeft:  "┴",
-		BottomRight: "┴",
-	}
-
-	tabStyle       lipgloss.Style = lipgloss.NewStyle().Border(tabBorder).Padding(0, 2)
-	activeTabStyle lipgloss.Style = tabStyle.Copy().Bold(true).Border(activeTabBorder, true)
-
-	tabGapStyle lipgloss.Style = tabStyle.Copy().BorderTop(false).BorderLeft(false).BorderRight(false)
-)
-
-func (t tui) header() string {
-	var todoTab, doneTab string
-
-	if t.view == todo {
-		todoTab = activeTabStyle.Render(string(todo))
-	} else {
-		todoTab = tabStyle.Render(string(todo))
-	}
-
-	if t.view == done {
-		doneTab = activeTabStyle.Render(string(done))
-	} else {
-		doneTab = tabStyle.Render(string(done))
-	}
-
-	tabs := lipgloss.JoinHorizontal(lipgloss.Bottom, todoTab, doneTab)
-
-	gap := tabGapStyle.Render(strings.Repeat(" ", max(0, t.w-lipgloss.Width(tabs)-2)))
-
-	return lipgloss.JoinHorizontal(lipgloss.Bottom, tabs, gap) + "\n\n"
-
-}
-
-func max(a, b int) int {
-	if a >= b {
-		return a
-	}
-	return b
-}
-
-func (t tui) View() string {
-	selected := lipgloss.NewStyle().Bold(true)
-	if len(t.renderSelection) == 0 { // init population
-		t.populateRenderSelection()
-	}
-
-	ret := t.header() // todo: stringbuilder
-	for i, item := range t.renderSelection {
-
-		if i == int(t.selection) {
-			ret += "> "
-			ret += selected.Render(item.String())
-		} else {
-			ret += "  "
-			ret += item.String()
-		}
-		ret += "\n"
-	}
-	return ret
-}
 
 func getItems(file string) []*tuido.Item {
 	prefixes := []string{"[ ]", "[@]", "[x]", "[~]", "[?]"}
---
+++ tui/view.go
@@ -0,0 +1,87 @@
+package tui
+
+import (
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	activeTabBorder = lipgloss.Border{
+		Top:         "─",
+		Bottom:      " ",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "┘",
+		BottomRight: "└",
+	}
+
+	tabBorder = lipgloss.Border{
+		Top:         "─",
+		Bottom:      "─",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "┴",
+		BottomRight: "┴",
+	}
+
+	tabStyle       lipgloss.Style = lipgloss.NewStyle().Border(tabBorder).Padding(0, 2)
+	activeTabStyle lipgloss.Style = tabStyle.Copy().Bold(true).Border(activeTabBorder, true)
+
+	tabGapStyle lipgloss.Style = tabStyle.Copy().BorderTop(false).BorderLeft(false).BorderRight(false)
+)
+
+func (t tui) header() string {
+	var todoTab, doneTab string
+
+	if t.view == todo {
+		todoTab = activeTabStyle.Render(string(todo))
+	} else {
+		todoTab = tabStyle.Render(string(todo))
+	}
+
+	if t.view == done {
+		doneTab = activeTabStyle.Render(string(done))
+	} else {
+		doneTab = tabStyle.Render(string(done))
+	}
+
+	tabs := lipgloss.JoinHorizontal(lipgloss.Bottom, todoTab, doneTab)
+
+	gap := tabGapStyle.Render(strings.Repeat(" ", max(0, t.w-lipgloss.Width(tabs)-2)))
+
+	return lipgloss.JoinHorizontal(lipgloss.Bottom, tabs, gap) + "\n\n"
+
+}
+
+func (t tui) View() string {
+	selected := lipgloss.NewStyle().Bold(true)
+	if len(t.renderSelection) == 0 { // init population
+		t.populateRenderSelection()
+	}
+
+	ret := t.header() // todo: stringbuilder
+	for i, item := range t.renderSelection {
+
+		if i == int(t.selection) {
+			ret += "> "
+			ret += selected.Render(item.String())
+		} else {
+			ret += "  "
+			ret += item.String()
+		}
+		ret += "\n"
+	}
+	return ret
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}

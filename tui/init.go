c710851a0b517fd199d946c9389a47b3986df86e
Colin Kennedy <colin@mrkennedy.ca>
2022-05-10 10:16:39 -0300

    add search for cfg at $USERCONFIGDIR/tuido.conf

--- tui/init.go
+++ tui/init.go
@@ -1,10 +1,47 @@
 package tui
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 func init() {
 	rand.Seed(time.Now().Unix()) // a fresh set of tag colors on each run. Spice of life.
+
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("error getting user home dir: %s", err)
+	}
+	tuidoDir := filepath.Join(dir, ".tuido")
+	runConfig.writeto = tuidoDir
+
+	loadFromDefaultConfigLocation()
+
+	// make sure the write target exists
+	_, err = os.Open(runConfig.writeto)
+	if err != nil {
+		err = os.Mkdir(runConfig.writeto, 0777)
+		if err != nil {
+			fmt.Printf("error creating appDirectory %s': %v\n",
+				runConfig.writeto, err)
+		}
+	}
+}
+
+func loadFromDefaultConfigLocation() {
+	cfgDir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Println("error seeking configdir")
+		return
+	}
+
+	cfgPath := filepath.Join(cfgDir, "tuido.conf")
+	cfg := parseConfigIfExists(cfgPath)
+
+	if cfg != nil {
+		runConfig = *cfg
+	}
 }

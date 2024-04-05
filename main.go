d8d0e2e10f0dad925a10d53191039dd592bc145a
Colin Kennedy <colin@mrkennedy.ca>
2024-04-04 22:19:51 -0300

    add a `--version` flag...
    can be used on `tag` commits to prevent mismatches.
    
    should also be used as a git hook

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"flag"
+	"os"
+
 	"github.com/nilock/tuido/tui"
+	"github.com/nilock/tuido/utils"
 )
 
 func main() {
+	vFlag := flag.Bool("version", false, "Print the version and exit")
+
+	flag.Parse()
+
+	if *vFlag {
+		v := utils.Version()
+		println(v)
+		os.Exit(0)
+	}
+
 	tui.Run()
 }
